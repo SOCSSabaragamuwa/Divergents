44563260a22b4ecb545571a597248de76c4d1946
sajith <skd4gp@gmail.com>
2020-02-01 17:22:06 +0530

    get the api up and running

--- eco/src/main/java/com/divergents/eco/controller/RestController.java
+++ eco/src/main/java/com/divergents/eco/controller/RestController.java
@@ -1,10 +1,15 @@
 package com.divergents.eco.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api")
 public class RestController {
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String GetApi(){
+        return "Ok";
+    }
 
 }
 
