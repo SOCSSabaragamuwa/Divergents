8fb3b5956dbb4e0fa87bb71b32dee3f09a58e821
RolandHewage <roland.hewage@gmail.com>
2020-02-02 02:15:51 +0530

    Add Vehicles

--- eco/src/main/java/com/divergents/eco/controller/RestController.java
+++ eco/src/main/java/com/divergents/eco/controller/RestController.java
@@ -3,6 +3,7 @@ package com.divergents.eco.controller;
 
 import com.divergents.eco.model.Officer;
 import com.divergents.eco.model.User;
+import com.divergents.eco.model.repository.VehicleRepository;
 import com.divergents.eco.service.IEcoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.validation.Valid;
 
+import static org.apache.logging.log4j.util.Strings.isBlank;
 
 
 @org.springframework.web.bind.annotation.RestController
@@ -44,6 +46,18 @@ public class RestController {
     @RequestMapping(value = "/officers", method = RequestMethod.POST)
     public ResponseEntity<Officer> AddNewOfficer(@Valid @RequestBody Officer officer) {
         return new ResponseEntity<>(iEcoService.AddNewOfficer(officer), HttpStatus.CREATED);
+    }
+
+    @Autowired
+    VehicleRepository vehicleRepository;
+
+    @RequestMapping(value = "/vehicles", method = RequestMethod.POST)
+    public ResponseEntity<String> AddNewVehicle(@RequestBody String vehicle_number ) {
+        if( !isBlank(vehicle_number) && vehicleRepository.isValidVehicleNumber(vehicle_number) ){
+            vehicleRepository.InsertVehicle(vehicle_number);
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 }
 
