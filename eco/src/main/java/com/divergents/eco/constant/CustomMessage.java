b7857c63b0bb51a649dc84fbc965c70cad8c6e34
sachini <sachipcranasinghe@gmail.com>
2020-02-02 20:44:59 +0530

    removed old project

--- eco/src/main/java/com/divergents/eco/constant/CustomMessage.java
+++
@@ -1,7 +0,0 @@
-package com.divergents.eco.constant;
-
-public class CustomMessage {
-    public static final String  mobileDevMessage="User creation failed because the mobile no is not set";
-    public static final String nicDeveMessage = "User creation failed because the nic no is not set";
-    public static final String pwdDevMessage = "User creation failed because password complexity requirement not met";
-}
--- eco/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-insert into user(nic,mobile) values('932390345v','+947585071');
-insert into user(nic,mobile) values('995628162v','+947018652');
